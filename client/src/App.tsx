0890cc08d49bfadfcc46f142c65f399f8a1f246b
Navadeep-Reddy <vnavadeepreddysatti@gmail.com>
2025-03-08 13:58:16 +0530

    Imported old NavBar successfully

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import MapPage from './pages/MapPage'
 import LocationProvider from './context/LocationContext'
+import HomePage from "./pages/HomePage"
 
 const App = () => {
   return (
     <LocationProvider>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<MapPage />}></Route>
+          <Route path="/route" element={<MapPage />}></Route>
+          <Route path= '/' element={<HomePage />}></Route>
         </Routes> 
       </ BrowserRouter>
     </LocationProvider>
--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -4,7 +4,7 @@ import Logo from '@/assets/LogoYellow.png'
 const Navbar: React.FC = () => {
   return (
     <nav>
-        <ul className='flex justify-between w-full bg-secBlue h-14 items-center space-x-4 top-0 fixed'>  
+        <ul className='flex justify-between w-full bg-secBlue h-16 items-center space-x-4 top-0 fixed z-10'>  
             <img src={Logo} className='h-14 px-4' / >
             <div className='flex space-x-4 px-10'>
                 <li className='font-medium text-WhiteText'>Home</li>
