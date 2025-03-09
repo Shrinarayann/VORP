22a0f03fd694a584d5c9e1413e3aedcb7bff3e59
Shrinarayan <shrinarayann2005@gmail.com>
2025-03-09 19:31:04 +0530

    Fixed api response from calculate_routes

--- server/app/controllers/vrp_solution.py
+++ server/app/controllers/vrp_solution.py
@@ -1,6 +1,6 @@
 
 from flask import jsonify
-from ..utils import distance_matrix,vrp_solver
+from ..utils import vrp_solver,distance_matrix
 
 def calculate_routes(data,request):
     if not request.is_json:
@@ -38,28 +38,84 @@ def calculate_routes(data,request):
         res=vrp_solver.solve_cvrp(data)
         print(res)
         # OR-Tools response
-    # response = {
-    #     'status': 'success',
-    #     'objective_value': 5,
-    #     'routes': [
-    #         {'vehicle_id': 0, 'route': [{'node': 0, 'load': 0}, {'node': 1, 'load': 50}, {'node': 0, 'load': 50}], 'distance': 2, 'load': 50},
-    #         {'vehicle_id': 1, 'route': [{'node': 0, 'load': 0}, {'node': 2, 'load': 75}, {'node': 3, 'load': 175}, {'node': 0, 'load': 175}], 'distance': 3, 'load': 175}
-    #     ],
-    #     'total_distance': 5,
-    #     'total_load': 225
-    # }
+        response = {
+        'status': 'success',
+        'objective_value': 5,
+        'routes': [
+            {'vehicle_id': 0, 'route': [{'node': 0, 'load': 0}, {'node': 1, 'load': 50}, {'node': 0, 'load': 50}], 'distance': 2, 'load': 50},
+            {'vehicle_id': 1, 'route': [{'node': 0, 'load': 0}, {'node': 2, 'load': 75}, {'node': 3, 'load': 175}, {'node': 0, 'load': 175}], 'distance': 3, 'load': 175}
+        ],
+        'total_distance': 5,
+        'total_load': 225
+    }
 
     # Transform response into desired format
         formatted_routes = {
-            int(route['vehicle_id']): [step['node'] for step in route['route']]
+            int(route['vehicle_id']): [locations[step['node']]+[step['load']] for step in route['route']]
             for route in res['routes']
         }
 
-        for i in formatted_routes:
-            formatted_routes[i]=[locations[j] for j in formatted_routes[i]]
+        formatted_routes={}
+        for route in res['routes']:
+            temp_list=[]
+            prev_load=0
+            for step in route['route'][:-1]:
+                temp_list.append(locations[step['node']]+[step['load']-prev_load])
+                prev_load=step['load']
+            formatted_routes[route['vehicle_id']]=temp_list
+
+        formatted_routes1 = {
+            route['vehicle_id']: {
+                tuple(locations[step['node']]): step['load']
+                for step in route['route']
+            }
+            for route in response['routes']
+        }
+
+# Print the formatted output
+
+        print('FORMATEED ROUTES 1 !!')
+        print(formatted_routes1)
+
+
+        # for i in formatted_routes:
+        #     formatted_routes[i]=[locations[j] for j in formatted_routes[i]]
+
+        formatted_routes2 = {
+            route['vehicle_id']: {
+                f"{locations[step['node']][0]}, {locations[step['node']][1]}": step['load']
+                for step in route['route']
+            }
+            for route in response['routes']
+        }
+        print('FORMATEDDDD ROUTESSSS 2222')
+        print (formatted_routes2)
         # Output the result
         print('\n\nFinal Calculated routes')
         print(formatted_routes)
+
+        # fr3 = {}
+
+        # for route in response['routes']:
+        #     vehicle_id = route['vehicle_id']
+        #     fr3[vehicle_id] = {}
+            
+        #     prev_load = 0  # To track the previous load
+        #     for step in route['route']:
+        #         node = step['node']
+        #         coordinates = locations[node]
+                
+        #         # Load picked up at this location (current - previous)
+        #         load_picked_up = step['load'] - prev_load
+        #         prev_load = step['load']  # Update for the next iteration
+                
+        #         # Store the data
+        #         fr3[vehicle_id][f"{coordinates[0]}, {coordinates[1]}"] = load_picked_up
+
+        # # Returning JSON
+        # return {"message": "Success", "calculated_routes": fr3}
+    
+
 
             # Return a valid response
         return {"message": "Success", "calculated_routes": formatted_routes}
