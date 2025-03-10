060825b7abb8891385a955160e4a00a25d4da68c
Navadeep-Reddy <vnavadeepreddysatti@gmail.com>
2025-03-10 22:14:41 +0530

    removed env variables

--- server/app/routes/auth_routes.py
+++ server/app/routes/auth_routes.py
@@ -1,10 +1,7 @@
 from flask import Blueprint, request, jsonify
 from supabase import create_client
-from dotenv import load_dotenv
-import os
-load_dotenv()
 
-supabase = create_client(os.getenv("SUPABASE_URL"), os.getenv("SUPABASE_KEY"))
+supabase = create_client("https://laqmmoixletufgzhgwkl.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxhcW1tb2l4bGV0dWZnemhnd2tsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE2MTI5NDMsImV4cCI6MjA1NzE4ODk0M30.iqwf5Z6Gmga9BUlv3br4nxG6RvqFvYRPoHcIAaYbodc")
 auth_bp = Blueprint("auth", __name__,url_prefix='/api/v1') 
 
 
